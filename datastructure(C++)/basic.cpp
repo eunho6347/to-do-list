86285acb6a655f7c8648da2e4343020a017baca6
박은호 <eunho.park6347@gmail.com>
2025-07-28 13:47:09 +0900

    🎉 첫 커밋 - To-Do 리스트

--- datastructure(C++)/basic.cpp
+++ datastructure(C++)/basic.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
-int main(){
-  cout << "Hello World!" << endl;
+
+int main() {
+    int N, tot = 5000, ans = -1;
+    cin >> N;
+    for(int x=0; x<N; x++){
+        for(int y=0; y<N; y++){
+            if(3*x + 5*y == N){
+                if(x+y < tot){
+                    tot = x+y;
+                    ans = tot;
+                }
+            }
+        }
+    }
+    cout << ans;
+    return 0;
 }
